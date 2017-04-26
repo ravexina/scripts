9435ba82aa27a84e8b18fc5cd7630a0c565bd553
ravexina <ravexina@gmail.com>
2017-04-26 15:12:51 +0430

    Remove unrelated comments

--- unmount.sh
+++ unmount.sh
@@ -3,28 +3,6 @@
 # Author: ravexina 
 # https://github.com/ravexina
 
-# Usage: 
-# 	Create a list of programs installed from a specific category
-#
-# Example: 
-#       `pkgs-from universe` will list all packages from universe category (component)
-#
-# There may be some false detection in "backports" !!
-
-# Licensed under the GNU General Public License v3.0 
-
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation; either version 3 of the License, or
-#  (at your option) any later version.
-#  This program is distributed in the hope that it will be useful,
-#  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#  GNU General Public License for more details.
-
-#  You should have received a copy of the GNU General Public License
-#  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 # Synchronize cached writes to persistent storage
 echo "start syncing..."
 sync
