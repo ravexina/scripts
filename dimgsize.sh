f05e6e2ae0987a34d92e50e057f4ebe6600c8b03
ravexina <ravexina@gmail.com>
2020-04-10 20:47:34 +0430

    Add ability to search for specific tag

--- dimgsize.sh
+++ dimgsize.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+if test -z "$2";
+then
+    TAG="latest"
+else
+    TAG="$2";
+fi
+
 export http_proxy=http://localhost:8118
 export https_proxy=http://localhost:8118
 
-export HUBUSER=
-export HUBPASS=
-export HUBTOKEN=$(curl -s -H "Content-Type: application/json" -X POST -d '{"username": "'${HUBUSER}'", "password": "'${HUBPASS}'"}' https://hub.docker.com/v2/users/login/ | jq -r .token)
-
-curl -s -H "Authorization: JWT ${TOKEN}" "https://hub.docker.com/v2/repositories/library/$1/tags/?page_size=100"\
- | jq -r '.results[] | select(.name == "latest") | .images[0].size' | numfmt --to=iec-i
+RESULT=$(curl -s -H "Authorization: JWT ${TOKEN}" "https://hub.docker.com/v2/repositories/library/$1/tags/?page_size=100")
+echo "$RESULT" | jq -r '.results[] | select(.name == "'$TAG'") | .images[] | select(.architecture == "amd64" and .os == "linux") | .size' | numfmt --to=iec-i
